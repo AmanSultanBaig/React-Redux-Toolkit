3317023198b0494d63f8d5aa20738aae194268ad
AmanSultanBaig <amansultanbaig@gmail.com>
2022-09-28 22:51:25 +0500

    Ui added to components

--- react-redux-toolkit-implementation/src/App.js
+++ react-redux-toolkit-implementation/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar'
+import Products from './components/Product'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Navbar/>
+      <Products/>
     </div>
   );
 }
