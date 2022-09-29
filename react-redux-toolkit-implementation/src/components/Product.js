a421a26e3455fa00f6bd5b4da215e4005fe121b3
AmanSultanBaig <amansultanbaig@gmail.com>
2022-09-29 23:10:51 +0500

    api integrated to product component

--- react-redux-toolkit-implementation/src/components/Product.js
+++ react-redux-toolkit-implementation/src/components/Product.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Product() {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(result => setProducts(result.data))
+  }, [])
+
+
   return (
     <>
-      <div className="card" style={{ width: "18rem", margin: "1rem" }}>
-        <img className="card-img-top" src="..." alt="Card image cap" />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
+      <div className='row'>
+        {
+          products.map(item => (
+            <div className='col-md-4 mt-3'>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title"><b>{item.name.toUpperCase()}</b></h5>
+                  <p className="-text">{item.email}</p>
+                  <button className='btn btn-primary'>Add {item.name}</button>
+                </div>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </>
   )
